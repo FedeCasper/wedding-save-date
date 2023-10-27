2d292af9c71a4e8e2e74e21f832a9168b8971c04
FedeCasper <federicorouyere@gmail.com>
2023-10-27 15:43:29 -0300

    Changing bg

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,18 @@
 /** @type {import('tailwindcss').Config} */
-tailwind.config = {
+
+const tailwindCustomConfig = {
    theme: {
          fontFamily: {
             poppins: 'Poppins, sans-serif',
             sans: 'Graphik, sans-serif'
          },
+         colors: {
+            'cream': '#EAE8E4',
+            'dark-gray': '#404040',
+            'mybrown': '#C49F5F',
+            'dry-green': '#5D7551',
+         }
    }
 }
+
+export default tailwindCustomConfig;
