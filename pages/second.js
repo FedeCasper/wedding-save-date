c95ed9b85385a18379cdfcccd32487688e56007a
FedeCasper <federicorouyere@gmail.com>
2023-10-31 19:23:26 -0300

    Working on addEvent on second hmtl

--- pages/second.js
+++ pages/second.js
@@ -1,4 +1,6 @@
 const $picContainer = document.getElementById('pics-container')
+const $footerButtons = document.querySelectorAll('.write-and-countdown')
+console.log($footerButtons);
 const $picCircle = document.getElementById('pics-circle')
 console.log($picCircle);
 const picOne = document.getElementById('first-pic')
@@ -33,11 +35,27 @@ $picContainer.addEventListener('touchstart', () => {
 })
 
 $picContainer.addEventListener('mouseenter', () => {
-      picOne.style.transform = 'translateY(-150px) translateX(-100%) rotate(-15deg)';
-      picThree.style.transform = 'translateY(150px) translateX(100%) rotate(15deg)';
+      picOne.style.transform = 'translateY(-150px) translateX(-75%) rotate(15deg)';
+      picThree.style.transform = 'translateY(150px) translateX(75%) rotate(-15deg)';
 })
 
 $picContainer.addEventListener('mouseleave', () => {
    picOne.style.transform = 'translateY(0) rotate(-15deg)'; 
    picThree.style.transform = 'translateY(0) rotate(15deg)'; 
 })
+
+$footerButtons.forEach(element => element.addEventListener('mouseover', (e) => {
+   if(e.target.classList[0]){
+      element.style.color = 'red'
+      element.style.fontWeight = 'bold'
+      element.style.transform = 'scale(1.2)'
+   }
+}))
+
+$footerButtons.forEach(element => element.addEventListener('mouseleave', (e) => {
+   if(e.target.classList[0]){
+      element.style.color = 'black'
+      element.style.fontWeight = 'normal'
+      element.style.transform = 'scale(1)'
+   }
+}))
