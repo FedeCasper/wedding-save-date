c08a2bda9e62101093b54ce99b57fc7ddac8accf
FedeCasper <federicorouyere@gmail.com>
2023-10-31 16:24:40 -0300

    Creating second.js

--- pages/second.js
+++ pages/second.js
@@ -5,17 +5,17 @@ const picOne = document.getElementById('first-pic')
 const picTwo = document.getElementById('second-pic')
 const picThree = document.getElementById('third-pic')
 
-$picContainer.addEventListener('mouseover', () => {
-   picOne.style.transform = 'translateY(-150px) translateX(-100%) rotate(-15deg)';
-   picThree.style.transform = 'translateY(150px) translateX(100%) rotate(15deg)';
-   $picCircle.style.transform = 'rotate(360deg)';
-})
+// $picContainer.addEventListener('mouseover', () => {
+//    picOne.style.transform = 'translateY(-150px) translateX(-100%) rotate(-15deg)';
+//    picThree.style.transform = 'translateY(150px) translateX(100%) rotate(15deg)';
+//    $picCircle.style.transform = 'rotate(360deg)';
+// })
 
-$picContainer.addEventListener('mouseleave', () => {
-   picOne.style.transform = 'translate(0) rotate(-15deg)'; 
-   picThree.style.transform = 'translate(0) rotate(15deg)'; 
-   $picCircle.style.transform = 'rotate(0deg)';
-});
+// $picContainer.addEventListener('mouseleave', () => {
+//    picOne.style.transform = 'translate(0) rotate(-15deg)'; 
+//    picThree.style.transform = 'translate(0) rotate(15deg)'; 
+//    $picCircle.style.transform = 'rotate(0deg)';
+// });
 // translateY(-150px) translateX(-100%) rotate(-15deg)
 
 $picContainer.addEventListener('touchstart', () => {
