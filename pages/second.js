30e86ec4a3b3ab688bd3410875791454d9ace0bd
FedeCasper <federicorouyere@gmail.com>
2023-10-31 16:22:41 -0300

    Creating second.js

--- pages/second.js
+++ pages/second.js
@@ -17,3 +17,15 @@ $picContainer.addEventListener('mouseleave', () => {
    $picCircle.style.transform = 'rotate(0deg)';
 });
 // translateY(-150px) translateX(-100%) rotate(-15deg)
+
+$picContainer.addEventListener('touchstart', () => {
+   picOne.style.transform = 'translateY(-150px) translateX(-100%) rotate(-15deg)';
+   picThree.style.transform = 'translateY(150px) translateX(100%) rotate(15deg)';
+   $picCircle.style.transform = 'rotate(360deg)';
+})
+
+$picContainer.addEventListener('touchend', () => {
+   picOne.style.transform = 'translate(0) rotate(-15deg)'; 
+   picThree.style.transform = 'translate(0) rotate(15deg)'; 
+   $picCircle.style.transform = 'rotate(0deg)';
+});
