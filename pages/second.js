51050770a7f7c49c6a1e43ac51f339de471e4192
FedeCasper <federicorouyere@gmail.com>
2023-10-31 16:09:21 -0300

    Creating second.js

--- pages/second.js
+++ pages/second.js
@@ -1,4 +1,6 @@
 const $picContainer = document.getElementById('pics-container')
+const $picCircle = document.getElementById('pics-circle')
+console.log($picCircle);
 const picOne = document.getElementById('first-pic')
 const picTwo = document.getElementById('second-pic')
 const picThree = document.getElementById('third-pic')
@@ -6,10 +8,12 @@ const picThree = document.getElementById('third-pic')
 $picContainer.addEventListener('mouseover', () => {
    picOne.style.transform = 'translateY(-150px) translateX(-100%) rotate(-15deg)';
    picThree.style.transform = 'translateY(150px) translateX(100%) rotate(15deg)';
+   $picCircle.style.transform = 'rotate(360deg)';
 })
 
 $picContainer.addEventListener('mouseleave', () => {
    picOne.style.transform = 'translate(0) rotate(-15deg)'; 
    picThree.style.transform = 'translate(0) rotate(15deg)'; 
+   $picCircle.style.transform = 'rotate(0deg)';
 });
 // translateY(-150px) translateX(-100%) rotate(-15deg)
