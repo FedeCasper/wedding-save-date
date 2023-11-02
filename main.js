8d8f046eaadd331e2aa858fb4712c814704eea03
FedeCasper <federicorouyere@gmail.com>
2023-11-02 19:56:11 -0300

    Working on details

--- main.js
+++ main.js
@@ -1,25 +1,23 @@
-const $footerBtns = document.querySelectorAll('.foot-btn');
-console.log($footerBtns);
-let url_1 = 'https://api.whatsapp.com/send?phone=2616189574'
+const $buttons = document.querySelectorAll('.btn');
+
+let url_1 = './pages/second.html'
 let url_2 = './pages/third.html'
+let url_3 = 'https://api.whatsapp.com/send?phone=2616189574'
 
-
-function annotateFn(element, url1, url2) {
-   const annotation = RoughNotation.annotate(element, { type: 'highlight', color: '#c49f5f5c', animationDuration: 500, strokeWidth: 5 });
-   annotation.show();
-   
-   setTimeout(() => {
-      element.outerText == 'Escribinos' ?
-      window.location.href = url1:
-      window.location.href = url2;
-      annotation.remove();
-   }, 700)
-}
-
-$footerBtns.forEach( button => {
-   button.addEventListener('click', ()=>{ annotateFn(button, url_1, url_2) } );
+$buttons.forEach( button => {
+   button.addEventListener('click', ()=>{ redirectionDelay(button, url_1, url_2, url_3) } );
 })
 
+function redirectionDelay(element, url1, url2, url3) {
+   if(element.outerText == 'Save the date'){
+      setTimeout( ( ) => { window.location.href = url1 }, 500);
+   }else if(element.outerText == 'Cuenta Regresiva'){
+      setTimeout( ( ) => { window.location.href = url2 }, 500);
+   } else{
+      setTimeout( ( ) => { window.open( url3, '_blank' ) }, 1000);
+   }
+
+}
 
 
 
