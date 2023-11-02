6b7b93cdb3dc5503bb4dced83a8bd4b14a6e6476
FedeCasper <federicorouyere@gmail.com>
2023-11-02 17:25:22 -0300

    Working on details

--- main.js
+++ main.js
@@ -1,14 +1,26 @@
-const e = document.querySelector('#foot-btn');
-console.log(e);
+const $footerBtns = document.querySelectorAll('.foot-btn');
+console.log($footerBtns);
+let url_1 = 'https://api.whatsapp.com/send?phone=2616189574'
+let url_2 = './pages/third.html'
 
-e.addEventListener('click', annotateFn);
-function annotateFn() {
-   console.log("object");
-   const annotation = RoughNotation.annotate(e, { type: 'highlight', color: '#c49f5f5c', animationDuration: 500, strokeWidth: 5 });
+
+function annotateFn(element, url1, url2) {
+   const annotation = RoughNotation.annotate(element, { type: 'highlight', color: '#c49f5f5c', animationDuration: 500, strokeWidth: 5 });
    annotation.show();
+   
    setTimeout(() => {
-      window.location.href = 'https://api.whatsapp.com/send?phone=2616189574';
+      element.outerText == 'Escribinos' ?
+      window.location.href = url1:
+      window.location.href = url2;
       annotation.remove();
    }, 700)
 }
+
+$footerBtns.forEach( button => {
+   button.addEventListener('click', ()=>{ annotateFn(button, url_1, url_2) } );
+})
+
+
+
+
 
