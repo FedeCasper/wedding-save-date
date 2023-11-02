9eda70e4feb983559a02067be0460fa637947c19
FedeCasper <federicorouyere@gmail.com>
2023-11-02 15:53:31 -0300

    Working on details

--- pages/second.js
+++ pages/second.js
@@ -1,26 +1,11 @@
 const $picContainer = document.getElementById('pics-container')
 const $footerButtons = document.querySelectorAll('.write-and-countdown')
-console.log($footerButtons);
 const $picCircle = document.getElementById('pics-circle')
-console.log($picCircle);
 const picOne = document.getElementById('first-pic')
 const picTwo = document.getElementById('second-pic')
 const picThree = document.getElementById('third-pic')
 
 let toogle = true
-
-// $picContainer.addEventListener('mouseover', () => {
-//    picOne.style.transform = 'translateY(-150px) translateX(-100%) rotate(-15deg)';
-//    picThree.style.transform = 'translateY(150px) translateX(100%) rotate(15deg)';
-//    $picCircle.style.transform = 'rotate(360deg)';
-// })
-
-// $picContainer.addEventListener('mouseleave', () => {
-//    picOne.style.transform = 'translate(0) rotate(-15deg)'; 
-//    picThree.style.transform = 'translate(0) rotate(15deg)'; 
-//    $picCircle.style.transform = 'rotate(0deg)';
-// });
-// translateY(-150px) translateX(-100%) rotate(-15deg)
 
 $picContainer.addEventListener('touchstart', () => {
    if(toogle){
@@ -59,3 +44,10 @@ $footerButtons.forEach(element => element.addEventListener('mouseleave', (e) => 
       element.style.transform = 'scale(1)'
    }
 }))
+
+function redirectionDelay() {
+   let url = 'https://www.google.com/calendar/event?action=TEMPLATE&text=Casamiento%20Fede%26Fede%20💒👉🏼🥳&dates=20240309/20240310&details=Nos%20casamos!%20y%20s%C3%AD,%20obviamente%20que%20est%C3%A1s%20invitado.%0A%0AA%20tener%20en%20cuenta%20%F0%9F%95%9C%F0%9F%91%87%F0%9F%8F%BC%0A%F0%9F%92%92El%20%C2%A1S%C3%AD!%20de%20la%20iglesia%20lo%20damos%20a%20las%2011%20a.m%0A%F0%9F%A5%82El%20brindis%20empieza%20a%20las%2013%20p.m%20en%20la%20finca%20AMproS%0A%F0%9F%8E%89%20La%20fiesta%20depende%20de%20ustedes,%20así%20que%20vayan%20con%20zapatos%20c%C3%B3modos%20para%20darlo%20todo.%0A%0ALos%20queremos%20%F0%9F%92%9C%0AF%26F&location=AMProS%20Finca,%20C.%20Pescara,%20Cruz%20de%20Piedra,%20Mendoza,%20Argentina'
+   setTimeout( ( ) => {
+         window.location.href = url;
+   }, 1000);
+}
